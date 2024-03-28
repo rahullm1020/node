cc68f52483066d8c7670d431959525e97815c1c5
rahullm1020 <158698240+rahullm1020@users.noreply.github.com>
2024-03-28 13:27:03 +0530

    Create calc.js

---
+++ calc.js
@@ -0,0 +1,15 @@
+exports.add = async(operator1, operator2) =>{
+    console.log("Result", operator1 + operator2)
+};
+
+exports.subtract = async(operator1, operator2) =>{
+    console.log("Result", operator1 - operator2)
+};
+
+exports.multiply = async(operator1, operator2) =>{
+    console.log("Result", operator1 * operator2)
+};
+
+exports.divide = async(operator1, operator2) =>{
+    console.log("Result", operator1 / operator2)
+};
